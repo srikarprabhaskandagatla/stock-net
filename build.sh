c3009d8331a01c386a0e673ee9be872851e25085
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-06-06 12:03:07 -0500

    Add Unit Test - Paxos

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ if [[ "$MODE" == "paxos" ]]; then
     echo "Using Compose file: $COMPOSE_FILE"
     docker-compose -f "$COMPOSE_FILE" build
     docker-compose -f "$COMPOSE_FILE" up client-service-paxos
+    # echo "Running  Tests."
+    docker-compose -f "$COMPOSE_FILE" up test
 else
     COMPOSE_FILE="docker-compose.yml"
     echo "Using Compose file: $COMPOSE_FILE"
     docker-compose -f "$COMPOSE_FILE" build
     docker-compose -f "$COMPOSE_FILE" up client-service
     # echo "Running  Tests."
-    docker-compose up test
+    docker-compose -f "$COMPOSE_FILE" up test
 fi
